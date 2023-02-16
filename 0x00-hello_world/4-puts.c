110f2178051a0c8c9f1484a94596c0ace3f48b2d
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-02-16 13:01:45 -0800

    4_puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 /**
- * This code is print ""Programming is like building a multilingual puzzle
- * return 0 is use the code run successfully
- ***/
+*main - Main function of my project
+*#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Main function of my project
+*
+*
+*Return: return 0 everytime
+*/
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
+*
+*Return: return 0 everytime
+*/
 int main(void) {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);

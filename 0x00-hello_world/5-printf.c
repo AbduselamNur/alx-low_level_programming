ad330d18a9550638a380311f6062abde58fd9e31
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-02-16 13:31:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/** This is the The C project
+ * This code is print with proper grammar, but the outcome is a piece of art,
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}

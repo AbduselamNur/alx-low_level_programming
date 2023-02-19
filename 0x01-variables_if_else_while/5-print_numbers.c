f6ca6d14072f86a491e6030ac35b72fef7f6c5ac
root <abduselamnurhussen626@gmail.com>
2023-02-19 00:19:14 -0800

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+/**
+ * main - void function
+ *  Description: print a number 0 to 10
+ *
+ *  Return: return zero
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
+

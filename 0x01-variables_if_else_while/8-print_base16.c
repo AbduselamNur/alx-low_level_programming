10291cf9506438ab4299a0ec0e6696332fec17fe
root <abduselamnurhussen626@gmail.com>
2023-02-19 06:00:13 -0800

    print-num,alpha

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * main - void fun
+ *
+ * Description: print num and alph
+ *
+ * Return: return zero
+ *
+ */
+
+int main(void) 
+{
+	int n;
+	char a;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

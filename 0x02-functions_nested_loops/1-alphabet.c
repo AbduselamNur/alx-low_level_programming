70f505a354e68f248d93bc08b77631b5722e5a1c
root <abduselamnurhussen626@gmail.com>
2023-02-22 03:54:00 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - _putchar func
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+
+{
+	char ca;
+
+	for (ca = 'a'; ca <= 'z'; ca++)
+		_putchar(ca);
+	_putchar('\n');
+	return (0);
+}

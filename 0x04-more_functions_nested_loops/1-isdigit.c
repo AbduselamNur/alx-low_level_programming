7e35c0f9a3e5ab9fb6a69a4e08f8fd7b90d0f8aa
root <abduselamnurhussen626@gmail.com>
2023-02-23 10:04:11 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - print digit
+ * @c: declare char
+ * Return: return 1 and 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else 
+		return (0);
+}
+

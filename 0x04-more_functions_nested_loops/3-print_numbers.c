9e5edee4dab10a4205371e2bdd42f69df2d404e7
root <abduselamnurhussen626@gmail.com>
2023-02-23 10:46:28 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number in new line
+ * return: return 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i < 10; i++)
+	_putchar(i);
+	_putchar('\n');
+}

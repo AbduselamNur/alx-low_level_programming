cf480511ba2b8e7f802eb908e78d48f386574580
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-03-08 12:37:16 -0800

    count-length-of-string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
  * _strlen_recursion - Returns Length of String
  * @s: string
@@ -9,5 +10,8 @@
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
+	{
 		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }

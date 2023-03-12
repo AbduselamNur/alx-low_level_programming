bc652bc7ff19b4fb1d929e44a18550d78b085f56
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-03-12 07:11:44 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - it is main functions that print index zero
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: return zero
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

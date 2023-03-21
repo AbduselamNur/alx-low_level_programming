6a1d4e32e8ce03d38d9014add63101dddc3671ec
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-03-21 00:21:44 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - discribe dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: own of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

f319783895eef7140dbfd7165247c8ad297d9502
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-03-21 05:27:08 -0700

    print char of dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,7 @@ if (d->name)
 printf("Name: %s\n", d->name);
 else
 printf("Name: (nil)");
-if (d->age)
 printf("Age: %f\n", d->age);
-else
-printf("Agee: (nil)");
 if (d->owner)
 printf("Owner: %s\n", d->owner);
 else

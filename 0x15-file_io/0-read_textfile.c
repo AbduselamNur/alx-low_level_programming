e15463e39ed40848c64d51e62335532151552880
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-04-11 07:19:43 -0700

    first commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,27 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * read_textfile - Read the File Text
- * @filenam: The Name of file of Read
+ * @filename: The Name of file of Read
  * @letters: The Number of Letter that READ
  *
- * Return: return 0, count 
+ * Return: return counter
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 size_t bytes_read = 0;
 FILE *file = fopen("filename", "r");
 char ch;
-  if (filename == NULL) {
-    return 0;
-  }
 
-  if (file == NULL) {
-    return 0;
-  }
-
-  while ((ch = fgetc(file)) != EOF && bytes_read < letters) {
-    putchar(ch);
-    bytes_read++;
-  }
-
-  fclose(file);
-
-  return bytes_read;
+if (filename == NULL)
+return (0);
+if (file == NULL)
+return (0);
+while ((ch = fgetc(file)) != EOF && bytes_read < letters)
+{
+putchar (ch);
+bytes_read++;
+}
+fclose(file);
+return (bytes_read);
 }

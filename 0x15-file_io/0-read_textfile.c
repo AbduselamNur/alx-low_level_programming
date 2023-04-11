4b107ee644b5c9d3df290ab263fa30437caf4016
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-04-10 22:49:50 -0700

    first-commt

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read the File Text
+ * @filenam: The Name of file of Read
+ * @letters: The Number of Letter that READ
+ *
+ * Return: return 0, count 
+ */
+ssize_t read_textfile(const char *filename, size_t letters) {
+size_t bytes_read = 0;
+FILE *file = fopen("filename", "r");
+char ch;
+  if (filename == NULL) {
+    return 0;
+  }
+
+  if (file == NULL) {
+    return 0;
+  }
+
+  while ((ch = fgetc(file)) != EOF && bytes_read < letters) {
+    putchar(ch);
+    bytes_read++;
+  }
+
+  fclose(file);
+
+  return bytes_read;
+}

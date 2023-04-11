47ec8884ef1aa7f76b9d8e422abcf0544427349f
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-04-11 07:39:22 -0700

    second commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - functions that create a file
+ * @filename: charactor
+ * @text_content: the second charactor
+ *
+ * Return: return 1 and -1
+ */
+int create_file(const char *filename, char *text_content) {
+	FILE *fp = fopen(filename, "w");
+  if (filename == NULL) {
+    return -1;
+  }
+
+  if (text_content == NULL) {
+    text_content = "";
+  }
+
+  if (fp == NULL) {
+    return -1;
+  }
+
+  fputs(text_content, fp);
+
+  fclose(fp);
+
+  return 1;
+}

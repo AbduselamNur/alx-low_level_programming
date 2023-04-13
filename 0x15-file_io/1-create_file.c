55e6e5cdf18a9a691360d629ad00377691dcc500
AbduselamNur <abduselamnurhussen626@gmail.com>
2023-04-12 21:34:18 -0700

    second

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 /**
  * create_file - functions that create a file
  * @filename: charactor
@@ -6,17 +7,27 @@
  *
  * Return: return 1 and -1
  */
-int create_file(const char *filename, char *text_content)
-{
-	FILE *fp = fopen("filename", "w");
+int create_file(const char *filename, char *text_content) {
+	FILE *fp = fopen(filename, "w");
 
-	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	if (fp == NULL)
-		return (-1);
-	fputs(text_content, fp);
-	fclose(fp);
-	return (1);
+  if (filename == NULL) {
+    return -1;
+  }
+
+  if (text_content == NULL) {
+    text_content = "";
+  }
+
+  if (fp == NULL) {
+    return -1;
+  }
+
+  if (fputs(text_content, fp) == EOF) {
+    fclose(fp);
+    return -1;
+  }
+
+  fclose(fp);
+
+  return 1;
 }
